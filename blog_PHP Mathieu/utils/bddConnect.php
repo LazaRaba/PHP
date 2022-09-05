6cc53ac21788a1fbea4c38660146911b451327ad
LazaRaba <103532996+LazaRaba@users.noreply.github.com>
2022-09-05 11:25:36 +0200

    Delete bddConnect.php

--- blog_PHP Mathieu/utils/bddConnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-   //connexion à la base de données
-   //Stockés dans une variable la connection, à la place de localhost on peut mettre l'adresse IP si c heberger en ligne
-   $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root','',// root = login par defaut de mysql
-   array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-?>
